ecbbd389f632e6161d513c02d1aa99d56cba9b7f
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-06-05 08:19:32 +0300

    Mean Square Error

---
+++ src/MeanSquareError/Solution.java
@@ -0,0 +1,24 @@
+/*Complete the function that
+
+        accepts two integer arrays of equal length
+        compares the value each member in one array to the corresponding member in the other
+        squares the absolute value difference between those two values
+        and returns the average of those squared absolute value difference between each member pair.
+        Examples
+        [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+        [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+        [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2*/
+
+package MeanSquareError;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static double solution(int[] arr1, int[] arr2) {
+        int[] arr3 = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr3[i] = arr1[i] - arr2[i];
+        }
+        return Arrays.stream(arr3).map(x-> x*x).summaryStatistics().getAverage();
+    }
+}
---
+++ src/MeanSquareError/SolutionTest.java
@@ -0,0 +1,37 @@
+package MeanSquareError;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+    @Test
+    public void test1() {
+        int[] a1 = {1, 2, 3};
+        int[] a2 = {4, 5, 6};
+        assertEquals(9.0, Solution.solution(a1, a2), 1e-15);
+    }
+
+    @Test
+    public void test2() {
+        int[] b1 = {10, 20, 10, 2};
+        int[] b2 = {10, 25, 5, -2};
+        assertEquals(16.5, Solution.solution(b1, b2), 1e-15);
+    }
+
+    @Test
+    public void test3() {
+        int[] c1 = {0, -1};
+        int[] c2 = {-1, 0};
+        assertEquals(1.0, Solution.solution(c1, c2), 1e-15);
+    }
+
+    @Test
+    public void test4() {
+        int[] d1 = {10, 10};
+        int[] d2 = {10, 10};
+        assertEquals(0.0, Solution.solution(d1, d2), 1e-15);
+    }
+}
